af5bbd3a7a25d5c9f901207b0fb56af245028f29
TJ-Mahady <tjmahady33@gmail.com>
2020-10-17 15:21:45 -0400

    Third commit

--- src/com/syntax/class08/AnotherTest.java
+++ src/com/syntax/class08/AnotherTest.java
@@ -3,8 +3,7 @@ package com.syntax.class08;
 public class AnotherTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Change made");
 	}
 
 }
