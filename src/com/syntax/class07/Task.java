53a04c27d2c6343745a15c60a731ca47fc1bc63d
TJ-Mahady <tjmahady33@gmail.com>
2020-10-18 09:22:34 -0400

    Fifth commit

---
+++ src/com/syntax/class07/Task.java
@@ -0,0 +1,26 @@
+package com.syntax.class07;
+
+public class Task {
+
+	public static void main(String[] args) {
+	
+		boolean workDay= true;
+		
+		int day=1;
+		
+		while(workDay) {
+			
+			if(day<6) {
+				
+				System.out.println("I need a day off");
+			}else {
+				System.out.println("It's the weekend, I am off");
+				workDay=false;
+			}
+			day++; 
+		}
+		
+
+	}
+
+}
