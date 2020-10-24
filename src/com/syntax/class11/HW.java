9105f6d134915daf772781b91671f148096fdf8b
TJ-Mahady <tjmahady33@gmail.com>
2020-10-24 10:58:03 -0400

    Class 11

---
+++ src/com/syntax/class11/HW.java
@@ -0,0 +1,19 @@
+package com.syntax.class11;
+
+public class HW {
+
+	public static void main(String[] args) {
+		
+		String[] animals = { "Lion", "Dog", "Rabbit", "Frog", "Octopus", "Snake" };
+	    for (int i = 0; i < animals.length; i++) {
+	      System.out.println(animals[i]);
+	    }
+	    
+	    
+	 
+	    
+	    
+
+	}
+
+}
---
+++ src/com/syntax/class11/HW3.java
@@ -0,0 +1,21 @@
+package com.syntax.class11;
+
+public class HW3 {
+
+	public static void main(String[] args) {
+
+		
+		String[] countries= {"USA","UK","France"};
+	    for (int i=0; i<countries.length; i++) {
+	      if(i==0) {
+	        System.out.println("D.C.");
+	      }else if(i==1) {
+	        System.out.println("London");
+	      }else {
+	        System.out.println("Paris");
+	      }
+	    }
+		
+	}
+
+}
